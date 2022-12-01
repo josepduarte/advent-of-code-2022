7f73eea120217c58fac9e1574b98be471f76a8ae
José Duarte <joseduarte@Joses-MBP.home>
2022-12-01 19:16:34 +0000

    Implement solution for day 1

---
+++ day_1/code.py
@@ -0,0 +1,26 @@
+############
+#  PART 1  #
+############
+elves_calories = [[]]
+with open('input.txt', 'r') as f:
+
+	line = f.readline()
+	while line:
+		try:
+			item_calories = int(line)
+			elves_calories[-1] += [item_calories]
+		except: 
+			# Empty line -> move to next bag/elf
+			elves_calories.append([])
+
+		line = f.readline()
+
+elves_calories_per_bag = [sum(ec) for ec in elves_calories]
+elves_calories_per_bag.sort()
+print(f'The Elf that is carrying more calories is carrying {elves_calories_per_bag[-1]} calories.')
+
+
+############
+#  PART 2  #
+############
+print(f'The top three elves that are carrying more calories are carrying {sum(elves_calories_per_bag[-3:])} calories.')
